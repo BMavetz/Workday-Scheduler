13ecd4bfca9a25c4a2a617686096311d3fd9b8c0
Brady Mavetz <b.mavetz@outlook.com>
2022-03-31 00:21:51 -0500

    HTML modified to add table, JS modified to allow for storing tasks to localStorage

---
+++ Develop/script.js
@@ -0,0 +1,62 @@
+var AM9text = $('#9AM').children('textarea');
+var AM10text = $('#10AM').children('textarea');
+var AM11text = $('#11AM').children('textarea');
+var PM12text = $('#12PM').children('textarea');
+var PM1text = $('#1PM').children('textarea');
+var PM2text = $('#2PM').children('textarea');
+var PM3text = $('#3PM').children('textarea');
+var PM4text = $('#4PM').children('textarea');
+var PM5text = $('#5PM').children('textarea');
+var curDay = moment().format('dddd, MMMM Do');
+$("#currentDay").text(curDay);
+
+function loadPage() {
+    AM9text.val(localStorage.getItem("9AM"));
+    AM10text.val(localStorage.getItem("10AM"));
+    AM11text.val(localStorage.getItem("11AM"));
+    PM12text.val(localStorage.getItem("12PM"));
+    PM1text.val(localStorage.getItem("1PM"));
+    PM2text.val(localStorage.getItem("2PM"));
+    PM3text.val(localStorage.getItem("3PM"));
+    PM4text.val(localStorage.getItem("4PM"));
+    PM5text.val(localStorage.getItem("5PM"));
+}
+
+$('#9AM').children('button').on('click', function () {
+    localStorage.setItem("9AM", AM9text.val());
+    
+})
+
+$('#10AM').children('button').on('click', function () {
+    localStorage.setItem("10AM", AM10text.val());
+})
+
+$('#11AM').children('button').on('click', function () {
+    localStorage.setItem("11AM", AM11text.val());
+})
+
+$('#12PM').children('button').on('click', function () {
+    localStorage.setItem("12PM", PM12text.val());
+})
+
+$('#1PM').children('button').on('click', function () {
+    localStorage.setItem("1PM", PM1text.val());
+})
+
+$('#2PM').children('button').on('click', function () {
+    localStorage.setItem("2PM", PM2text.val());
+})
+
+$('#3PM').children('button').on('click', function () {
+    localStorage.setItem("3PM", PM3text.val());
+})
+
+$('#4PM').children('button').on('click', function () {
+    localStorage.setItem("4PM", PM4text.val());
+})
+
+$('#5PM').children('button').on('click', function () {
+    localStorage.setItem("5PM", PM5text.val());
+})
+
+loadPage();
